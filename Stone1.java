6ac131eb89f64b5fd587d8be4458cfb43c8a10d4
joe2867 <jskelly@stu.socsd.org>
2024-10-08 15:02:14 -0400

    Update Stone1.java

--- Stone1.java
+++ Stone1.java
@@ -1,19 +1,33 @@
 public class Stone1{
     public static void main(String[] args){
         int n;
-        for(int i = 1; i < 200;){
+        int count = 0;
+        boolean loop;
+        int most = 0;
+        for(int i = 1; i < 201; i++){
             n = i;
-            if(n == 4){
-                i += 1;
-            }else{
-                if((n % 2) == 1){
-                    n = (n * 3) + 1;
-                } else{
-                    n = n / 2;
+            loop = false;
+            count = 0;
+            while(!loop ){
+                if(n == 4){
+                    System.out.println(i + ": " + count + " loops");
+                    if(count > most){
+                        most = count;
+                    }
+                    loop = true;
+                }
+                else{
+                    count = count + 1;
+                    if((n % 2) == 1){
+                        n = (n * 3) + 1;
+                    } else{
+                        n = n / 2;
+                    }
                 }
             }
-        
         }
+        System.out.println("The most amount of loops were " + most + " loops");
+
     }
-        
 }
+        
